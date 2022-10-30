bbd4cfd9582a152c5847a7c4ea1fa79835b4eea5
Ejim Favour <favourejim56@gmail.com>
2022-10-30 01:07:50 +0100

    slack and github logos added

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,6 +1,9 @@
 import LinksList from "../LinksList/LinksList";
 import Profile from "../Profile/Profile";
 import Footer from "../Footer/Footer";
+import classes from "./Layout.module.scss";
+import slackImg from "../../assets/slack.svg";
+import gitHubImg from "../../assets/Social icon.svg";
 
 const Layout = () => {
   return (
@@ -8,6 +11,14 @@ const Layout = () => {
       <Profile />
       <main>
         <LinksList />
+        <div className={classes["social-links"]}>
+          <a href="https://app.slack.com/client/T042F7V19Q8/setup-welcome">
+            <img src={slackImg} alt={"slack-logo"} />
+          </a>
+          <a href="https://github.com/ejim11">
+            <img src={gitHubImg} alt={"github-logo"} />
+          </a>
+        </div>
       </main>
       <Footer />
     </>
