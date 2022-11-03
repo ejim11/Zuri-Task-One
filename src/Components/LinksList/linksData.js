f0562f4ebe0441c9bac80efbad2582fbeb945d98
Ejim Favour <favourejim56@gmail.com>
2022-11-03 15:24:27 +0100

    refactor: linksData.js file refactored, links type  and contact object added

--- src/Components/LinksList/linksData.js
+++ src/Components/LinksList/linksData.js
@@ -4,24 +4,28 @@ const linksData = [
     link: `https://training.zuri.team/`,
     id: `btn__zuri`,
     text: "Get familiar with Zuri Team",
+    type: "external",
   },
   {
     title: "Zuri Books",
     link: `http://books.zuri.team`,
     id: `books`,
     text: "Access and explore the Zuri Library",
+    type: "external",
   },
   {
     title: "Python Books",
     link: `https://books.zuri.team/python-for-beginners?ref_id=@jimmy`,
     id: `book__python`,
     text: "Are you a beginner? explore numerous python books at cheap cost",
+    type: "external",
   },
   {
     title: "Background Checks for Coders",
     link: `https://background.zuri.team`,
     id: `pitch`,
     text: "Ensure your developer(s) are grounded",
+    type: "external",
   },
 
   {
@@ -29,6 +33,14 @@ const linksData = [
     link: `https://training.zuri.team/`,
     id: `book__design`,
     text: "Improve your design skills by exploring free books on elegant design",
+    type: "external",
+  },
+  {
+    title: "Contact Me",
+    link: "/contact",
+    id: "contact",
+    text: "Interested in working with me and creating innovative designs, contact me.",
+    type: "internal",
   },
 ];
 
